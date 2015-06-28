33e3f76e43d72e1e8b50e4c285bcd8b47b7bb1ba
alexandre-mazel <alexandre.zelma@gmail.com>
2015-06-28 23:26:54 +0200

    pick image

--- Glasses/app/src/main/java/com/electronoos/glasses/MyActivity.java
+++ Glasses/app/src/main/java/com/electronoos/glasses/MyActivity.java
@@ -2,8 +2,12 @@ package com.electronoos.glasses;
 
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -18,6 +22,7 @@ import android.util.Log;
 import android.content.Intent;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.VerticalSeekBar;
@@ -37,6 +42,8 @@ public class MyActivity extends ActionBarActivity /*MyShortcuts*/ {
     public final static String EXTRA_MESSAGE = "com.electronoos.glasses.MESSAGE";
 
     public final static String strClassName = "MyActivity";
+
+    private static int RESULT_LOAD_IMAGE = 1;
 
 
     private SeekBar seekBar_age_;
@@ -239,6 +246,17 @@ public class MyActivity extends ActionBarActivity /*MyShortcuts*/ {
         setLocale("fr");
     }
 
+    public void change_aoc(View view) {
+        // Do something in response to button
+        logger_.l( strClassName, "change_aoc: begin");
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+        logger_.l( strClassName, "change_aoc: end");
+    }
+
 
     public void seek_age_changed(SeekBar seekBar, int progress, boolean fromUser) {
         // Do something in response to seekbar change
@@ -274,6 +292,34 @@ public class MyActivity extends ActionBarActivity /*MyShortcuts*/ {
     public int getAcidity()
     {
         return seekBar_acidity_.getProgress();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            logger_.l( strClassName, "onActivityResult: picture_selected: " + picturePath );
+
+            ImageView imageView = (ImageView) findViewById(R.id.aoc_view);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+            logger_.l( strClassName, "onActivityResult: end"  );
+
+        }
+
+
     }
 
 }
