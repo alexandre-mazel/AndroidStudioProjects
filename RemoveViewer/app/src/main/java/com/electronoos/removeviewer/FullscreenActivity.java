ffc630c386023c609b1c94b99b5fae885c432695
alexandre-mazel <alexandre.zelma@gmail.com>
2015-11-28 23:49:38 +0100

    up

--- RemoveViewer/app/src/main/java/com/electronoos/removeviewer/FullscreenActivity.java
+++ RemoveViewer/app/src/main/java/com/electronoos/removeviewer/FullscreenActivity.java
@@ -4,11 +4,23 @@ import com.electronoos.removeviewer.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 /**
@@ -113,6 +125,9 @@ public class FullscreenActivity extends Activity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        Log.e( "RemoteViewer: onCreate", "before downloading...");
+        new DownloadFileFromURL().execute("http://perso.ovh.net/~mangedisf/mangedisque/images/bg_klee.gif", this);
     }
 
     @Override
@@ -157,4 +172,106 @@ public class FullscreenActivity extends Activity {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
+
+    public void showImage( String strPicturePath )
+    {
+        Log.v( "Viewer", "onActivityResult: picture_selected: " + strPicturePath );
+
+        ImageView imageView = (ImageView) findViewById(R.id.aoc_view);
+        imageView.setImageBitmap(BitmapFactory.decodeFile(strPicturePath));
+    }
+}
+
+
+/**
+ * Background Async Task to download file
+ * */
+class DownloadFileFromURL extends AsyncTask<String, String, String> {
+
+    private Activity parentActivity_; // parent that launch this request
+    private String strImage_; // destination filename
+
+    /**
+     * Before starting background thread Show Progress Bar Dialog
+     * */
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        // showDialog(progress_bar_type);
+    }
+
+    /**
+     * Downloading file in background thread
+     * */
+    @Override
+    protected String doInBackground(String f_url,Activity activity) {
+        int count;
+        parentActivity_ = activity;
+        try {
+            Log.e( "RemoteViewer: doInBackground", "beginning");
+            URL url = new URL(f_url[0]);
+            URLConnection conection = url.openConnection();
+            conection.connect();
+
+            // this will be useful so that you can show a tipical 0-100%
+            // progress bar
+            int lenghtOfFile = conection.getContentLength();
+
+            // download the file
+            InputStream input = new BufferedInputStream(url.openStream(),
+                    8192);
+
+            // Output stream
+            strImage_ = Environment.getExternalStorageDirectory().toString() + "/test.jpg";
+            OutputStream output = new FileOutputStream(strImage);
+
+            byte data[] = new byte[1024];
+
+            long total = 0;
+
+            while ((count = input.read(data)) != -1) {
+                total += count;
+                // publishing the progress....
+                // After this onProgressUpdate will be called
+                //publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+
+                // writing data to file
+                output.write(data, 0, count);
+            }
+
+            // flushing output
+            output.flush();
+
+            // closing streams
+            output.close();
+            input.close();
+
+        } catch (Exception e) {
+            Log.e("RemoteViewer: error: while downloading: ", e.getMessage());
+        }
+
+        return null;
+    }
+
+    /**
+     * Updating progress bar
+     * */
+    protected void onProgressUpdate(String... progress) {
+        // setting progress percentage
+        //pDialog.setProgress(Integer.parseInt(progress[0]));
+        Log.e( "RemoteViewer: ", progress[0]);
+    }
+
+    /**
+     * After completing background task Dismiss the progress dialog
+     * **/
+    @Override
+    protected void onPostExecute(String file_url) {
+        // dismiss the dialog after the file was downloaded
+        //dismissDialog(progress_bar_type);
+        Log.e( "RemoteViewer: ", "finito!");
+        parentActivity_.showImage( strImage );
+
+    }
+
 }
