88ddc468db5b5341ce614c145373168c817e6c11
alexandre-mazel <alexandre.zelma@gmail.com>
2017-03-07 22:40:07 +0100

    up new classes for sensors

---
+++ Blangle/app/src/main/java/com/electronoos/blangle/DisplaySensorActivityABC.java
@@ -0,0 +1,479 @@
+package com.electronoos.blangle;
+
+import com.electronoos.blangle.util.Averager;
+import com.electronoos.blangle.util.GetUserInput;
+import com.electronoos.blangle.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class Menu extends Activity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * If set, will toggle the system UI visibility upon interaction. Otherwise,
+     * will show the system UI visibility upon interaction.
+     */
+    private static final boolean TOGGLE_ON_CLICK = true;
+
+    /**
+     * The flags to pass to {@link SystemUiHider#getInstance}.
+     */
+    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+    /**
+     * The instance of the {@link SystemUiHider} for this activity.
+     */
+    private SystemUiHider mSystemUiHider;
+
+//    private BluetoothAdapter mBluetoothAdapter;
+    private SensorsManager   mSensorsManager;
+
+    // interface element
+    private TextView mTxtDeviceStatus;
+    private TextView mTxtBpm;
+    private TextView[] maTxtAngle;
+
+
+
+    private String mstrStatus;
+    private int mnBpm; // used to refresh in the good thread
+
+    final int mnNbrAngle = 3;
+    private double[] marAngle; // used to refresh in the good thread
+    private double[] maTimeLastUpdateMs;
+    private Averager[] maAngleAverage;
+    private int mnNbrUpdateBpm;
+    private String mstrLastTxt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v("DBG", "------------------------------");
+
+        super.onCreate(savedInstanceState);
+
+        Global.setCurrentActivity( this );
+
+        setContentView(R.layout.activity_menu);
+
+        final View controlsView = findViewById(R.id.fullscreen_content_controls);
+        final View contentView = findViewById(R.id.fullscreen_content);
+
+        mTxtBpm = (TextView) findViewById(R.id.menu_bpm);
+        maTxtAngle = new TextView[mnNbrAngle];
+        maTxtAngle[0] = (TextView) findViewById(R.id.menu_angle1);
+        maTxtAngle[1] = (TextView) findViewById(R.id.menu_angle2);
+        maTxtAngle[2] = (TextView) findViewById(R.id.menu_angle3);
+        mTxtDeviceStatus = (TextView) findViewById(R.id.menu_txt_device_status);
+
+        mnBpm = 0;
+        marAngle = new double[mnNbrAngle];
+        maTimeLastUpdateMs = new double[mnNbrAngle];
+        maAngleAverage = new Averager[mnNbrAngle];
+        for( int i = 0; i < mnNbrAngle; ++i) {
+            Log.v("DBG", "i: " + i );
+            maAngleAverage[i] = new Averager<Double>(30);
+        }
+        mnNbrUpdateBpm = 0;
+        mstrLastTxt = "";
+
+
+        // Set up an instance of SystemUiHider to control the system UI for
+        // this activity.
+        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
+        mSystemUiHider.setup();
+        mSystemUiHider
+                .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
+                    // Cached values.
+                    int mControlsHeight;
+                    int mShortAnimTime;
+
+                    @Override
+                    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+                    public void onVisibilityChange(boolean visible) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+                            // If the ViewPropertyAnimator API is available
+                            // (Honeycomb MR2 and later), use it to animate the
+                            // in-layout UI controls at the bottom of the
+                            // screen.
+                            if (mControlsHeight == 0) {
+                                mControlsHeight = controlsView.getHeight();
+                            }
+                            if (mShortAnimTime == 0) {
+                                mShortAnimTime = getResources().getInteger(
+                                        android.R.integer.config_shortAnimTime);
+                            }
+                            controlsView.animate()
+                                    .translationY(visible ? 0 : mControlsHeight)
+                                    .setDuration(mShortAnimTime);
+                        } else {
+                            // If the ViewPropertyAnimator APIs aren't
+                            // available, simply show or hide the in-layout UI
+                            // controls.
+                            controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
+                        }
+
+                        if (visible && AUTO_HIDE) {
+                            // Schedule a hide().
+                            delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                        }
+                    }
+                });
+
+        // Set up the user interaction to manually show or hide the system UI.
+        contentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TOGGLE_ON_CLICK) {
+                    mSystemUiHider.toggle();
+                } else {
+                    mSystemUiHider.show();
+                }
+            }
+        });
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        ((TextView) findViewById(R.id.fullscreen_content)).setText("");
+        //((TextView) findViewById(R.id.app_name)).setText("");
+        setTitle("");
+        getActionBar().setIcon(R.mipmap.ic_blank);
+        getActionBar().hide();
+
+        ////////////////////////////////////:
+        /// BLE stuffs
+        Log.v("DBG", "BLE check");
+
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Log.v("DBG", "NO BLE !!!");
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        Log.v("DBG", "BLE check - end");
+
+        postConnectBLE(2000);
+        postRefreshInterface(2000);
+
+    } // onCreate
+
+    @Override
+    protected void onPause() {
+        // we want this application to be stopped when set on background
+        Log.v("DBG", "------------------------------ onPause...");
+        super.onPause();
+        //onStop();
+        //System.exit(0); // exit this application
+    }
+    @Override
+    protected void onStop() {
+        if( true ) {
+            Log.v("DBG", "------------------------------ onStop...");
+            //mSensorsManager.exit();
+            //mSensorsManager = null;
+
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    Handler mHideHandler = new Handler();
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mSystemUiHider.hide();
+        }
+    };
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        return;
+        /*
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.menu_radio_choice_def:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.menu_radio_choice_calc:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+        */
+    }
+
+    public void onChoiceSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+//        startActivityForResult(intent, 1);
+    }
+
+    public void onChoiceDef(View view) {
+        Intent intent = new Intent(this, Definition.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void onChoiceCalc(View view) {
+        // Kabloey
+    }
+
+
+    public void onButtonReconnect(View view) {
+        postConnectBLE(10);
+    }
+
+    private void connectBLE() {
+        Log.v("DBG", "start BLE stuffs");
+        mTxtDeviceStatus.setText("Searching...");
+        if( true ) {
+            if( Global.getCurrentSensorsManager() == null ) {
+                mSensorsManager = new SensorsManager();
+                Global.setCurrentSensorsManager(mSensorsManager);
+                mSensorsManager.init();
+            }
+            mSensorsManager.discover();
+            postRefreshBLE(1000);
+        }
+        if( false ) {
+            Intent myIntent = new Intent(Menu.this, DeviceScanActivity.class);
+            //myIntent.putExtra("key", value); //Optional parameters
+            Menu.this.startActivity(myIntent);
+        }
+        Log.v("DBG", "start BLE stuffs - end");
+    }
+
+
+
+    private void postConnectBLE(int interval)
+    {
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable(){
+            public void run() {
+                Menu.this.connectBLE();
+            }
+        };
+        handler.postAtTime(runnable, System.currentTimeMillis()+interval);
+        handler.postDelayed(runnable, interval);
+    }
+
+    private void refreshBLE() {
+        //Log.v("DBG", "refreshBLE");
+        if( mSensorsManager != null )
+            mSensorsManager.update();
+        postRefreshBLE( 1000 );
+        //Log.v("DBG", "refreshBLE - end");
+    }
+
+    private void postRefreshBLE(int interval)
+    {
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable(){
+            public void run() {
+                Menu.this.refreshBLE();
+            }
+        };
+        handler.postAtTime(runnable, System.currentTimeMillis()+interval);
+        handler.postDelayed(runnable, interval);
+    }
+
+    public void updateStatus(String strStatus) {
+        mstrStatus = strStatus;
+    }
+
+    public void updateBpm(int nBpm)
+    {
+        //Log.v("DBG", "in txtview update !!!: " + nBpm);
+        //mTxtBpm.setText(String.valueOf(nBpm));
+        mnBpm = nBpm; // to be refreshed later
+        //postRefreshBpm( 10 ); // Can't create handler inside thread that has not called Looper.prepare()
+
+        // output to file
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String currentDateandTime = sdf.format(new Date());
+        String newLine = currentDateandTime + ": " + String.valueOf(nBpm);
+        Log.v( "DBG", newLine );
+        mstrLastTxt += newLine + "\n";
+
+        mnNbrUpdateBpm += 1;
+        if( mnNbrUpdateBpm > 60 ) {
+            try{
+                Log.v("DBG", "updateBpm: outputting to file!!!");
+                File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                File file = new File(path, "alex_hr.txt");
+
+                FileOutputStream fOut = new FileOutputStream(file, true ); // true for append
+                fOut.write(mstrLastTxt.getBytes());
+                fOut.close();
+            }
+            catch (Exception e){
+                Log.v("DBG", "updateBpm: Exception: disk error: " + e.toString());
+            }
+            mnNbrUpdateBpm = 0;
+            mstrLastTxt = "";
+        }
+
+    }
+    public void updateAngle(String strDeviceName, double rAngle)
+    {
+        //mTxtBpm.setText(String.valueOf(nBpm));
+        //mrAngle = rAngle; // to be refreshed later
+        rAngle -= 17.9; // results: 161.6 / 162.0 / 162.3 / 160.2 / 158.6 / 153.8 / 168.1 / 168.4 / 168.0 / 166.5 / 167.3 / 165.6 / 167.0 / 167.0 / 165.1 / 167.
+        int nCurrentIdx = Global.getSensorIdx(strDeviceName);
+        maAngleAverage[nCurrentIdx].addValue(rAngle);
+        marAngle[nCurrentIdx] = maAngleAverage[nCurrentIdx].computeAverage().doubleValue();
+    }
+
+    private void refreshInterface() {
+        //Log.v("DBG", "in refreshBpm update !!!: mnBpm:" + mnBpm);
+        if( mstrStatus != null ) {
+            mTxtDeviceStatus.setText(mstrStatus);
+            mstrStatus = null; // could miss one from time to time
+        }
+        if( mnBpm != 0 ) {
+            mTxtBpm.setText(String.valueOf(mnBpm));
+            mnBpm = 0; // could miss one from time to time
+        }
+        for( int i = 0; i < mnNbrAngle; ++i) {
+            if (marAngle[i] != 0.) {
+                maTxtAngle[i].setText(String.format("%.1f", marAngle[i]) + "°");
+                marAngle[i] = 0.; // could miss one from time to time
+                if( maTimeLastUpdateMs[i] <= -1 ) {
+                    maTxtAngle[i].setTextColor(Color.BLACK);
+                }
+                maTimeLastUpdateMs[i] = System.currentTimeMillis();
+            }
+            else
+            {
+                if( maTimeLastUpdateMs[i] > -1 )
+                {
+                    if( System.currentTimeMillis() - maTimeLastUpdateMs[i] > 2000 ) {
+                        maTimeLastUpdateMs[i] = -1;
+                        maTxtAngle[i].setTextColor(Color.RED);
+                    }
+                }
+            }
+        }
+
+        postRefreshInterface(500);
+
+    }
+
+    private void postRefreshInterface(int interval)
+    {
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable(){
+            public void run() {
+                Menu.this.refreshInterface();
+            }
+        };
+        handler.postAtTime(runnable, System.currentTimeMillis()+interval);
+        handler.postDelayed(runnable, interval);
+    }
+
+    public void askUser() {
+//
+//        runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                if (!isFinishing()) {
+//                    new AlertDialog.Builder(Global.getCurrentActivity())
+//                            .setTitle("Your Alert")
+//                            .setMessage("Your Message")
+//                            .setCancelable(false)
+//                            .show();
+//                }
+//                Log.v("DBG", "askText: returning: 'debugbugu'" );
+//
+////                GetUserInput.askText("New sensor detected, name it please:");
+//            }
+//        });
+        //GetUserInput.ask(this);
+    }
+}
